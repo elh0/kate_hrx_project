f412eb94bb8fc8488ea46d0ea687d57c4ec22c6b
Elliot <elliotholbrow@gmail.com>
2019-08-09 18:44:21 +0100

    Added keyup event

--- pages.js
+++ pages.js
@@ -3,11 +3,23 @@ let pageNumber = 0;
 
 // have the content for these pages
 const pages = [
-    { copy: "a street dancer", background: "#edc7a9", circle: "#3e78ed" },
-    { copy: "looking for a job in Brand Marketing", background: "#a1fffe", circle: "#e34a47"  },
-    { copy: `on <a href="pdf.pdf">linkedIn</a>`, background: "#d3c7f3", circle: "#f7fe00"  },
-    { copy: `letting you <a href="pdf.pdf">download her PDF</a>`, background: "#faffb8", circle: "#b472e6"  }
-]
+  { copy: "a street dancer", background: "#edc7a9", circle: "#3e78ed" },
+  {
+    copy: "looking for a job in Brand Marketing",
+    background: "#a1fffe",
+    circle: "#e34a47"
+  },
+  {
+    copy: `on <a href="pdf.pdf">linkedIn</a>`,
+    background: "#d3c7f3",
+    circle: "#f7fe00"
+  },
+  {
+    copy: `letting you <a href="pdf.pdf">download her PDF</a>`,
+    background: "#faffb8",
+    circle: "#b472e6"
+  }
+];
 
 // pick the relevant tags
 const nextTag = document.querySelector("footer img.next");
@@ -33,15 +45,15 @@ const previous = function() {
   pageNumber = pageNumber - 1;
 
   if (pageNumber < 0) {
-    pageNumber = pages.length - 1
+    pageNumber = pages.length - 1;
   }
 
   updateSection();
 };
 
 // pick a random slide
-const random = function () {
-  pageNumber = Math.floor(Math.random() * pages.length)
+const random = function() {
+  pageNumber = Math.floor(Math.random() * pages.length);
 
   updateSection();
 };
@@ -49,8 +61,8 @@ const random = function () {
 // this will update the section's content and style
 const updateSection = function() {
   outputTag.innerHTML = pages[pageNumber].copy;
-  circleTag.style.backgroundColor = pages[pageNumber].circle
-  bodyTag.style.backgroundColor = pages[pageNumber].background
+  circleTag.style.backgroundColor = pages[pageNumber].circle;
+  bodyTag.style.backgroundColor = pages[pageNumber].background;
 };
 
 // on click of nextTag, run this
@@ -63,8 +75,23 @@ previousTag.addEventListener("click", function() {
   previous();
 });
 
-
 // on click of random.Tag run this
 randomTag.addEventListener("click", function() {
   random();
+});
+
+// when a user a key, check for arrow left or right
+// and do next or prev correctly
+document.addEventListener("keyup", function(event) {
+  console.log(event);
+
+  // if the key being pressed is ArrowRight
+  if (event.key == "ArrowRight") {
+    next();
+  }
+
+  // if the key being pressed is ArrowLeft
+  if (event.key == "ArrowLeft") {
+    previous();
+  }
 });
